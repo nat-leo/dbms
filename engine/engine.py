7c4a3ff4358749d818180d0c228150c21d0d777f
natleo <n8wyliu@gmail.com>
2023-08-13 13:47:57 -0700

    fix working directory to create the db folder in the same dir as engine.py, also do that on initialization of the db engine so that users don't need to specify a path.

--- engine/engine.py
+++ engine/engine.py
@@ -10,7 +10,7 @@ class Table:
 class Database:
     def __init__(self, name) -> None:
         self.name = name
-        self.tables = {}
+        self.tables = {} # dict so we have constant access time.
 
     def add_table(self, table: Table):
         self.tables[table.name] = table
@@ -24,9 +24,10 @@ class Database:
 
 class DatabaseEngine:
     # stores all databases.
-    def __init__(self, directory) -> None:
-        self.directory = directory # for testing purposes. Either "os" (production) or "tmp_dir" (when testing)
-        self.databases = [] # list of Database objects
+    def __init__(self) -> None:
+        self.directory = os.path.join(os.path.dirname(__file__), "db") # create 'db' folder in the same dir as this file!
+        self.databases = {} # dict for constant access time 
+        os.mkdir(self.directory) # folder 'db' persists when program ends.
 
     ''' Given a set of instructions as a list, execute each
     operation from the first element to the last element.
@@ -65,33 +66,56 @@ class DatabaseEngine:
         return rows
     
     # create a table as a .bin file of the form table_name.bin
-    def create_table(self, db: str, table_name: str):
-        open(f"{self.directory}/{db}/{table_name}.bin", "wb")
+    def create_table(self, db_name: str, table_name: str, schema: dict):
+        # create the table as a file
+        path = f"{self.directory}/{db_name}/{table_name}.bin"
+        open(path, "wb")
+        
+        # register the table in the right db
+        table = Table(table_name, schema)
+        self.databases[db_name].add_table(table)
+
+        logging.info(f"DatabaseEngine: created Table Object {path}")
 
     # create a database as a directory, where the directory/folder name is db_name
     def create_db(self, db_name: str):
-        os.mkdir(self.directory+"/"+db_name)
-        self.databases.append(db_name)
+        # write the directory
+        path = os.path.join(self.directory, db_name)
+        os.mkdir(path)
+        # register a db object so we can keep track of dbs and tables easier.
+        db = Database(db_name)
+        self.databases[db_name] = db
+    
+        logging.info(f"DatabaseEngine: created Database object {path}")
     
     # completely delete the table from the database
     def drop_table(self, db_name: str, table_name: str):
-        # search list of dbs
-        #for i in range(len(self.databases)):
-            #if self.databases[i].name == db_name:
-                # search list of tables
-                # for j in range(len(self.databases)):
-                #    if self.databases[i].tables[j] == table_name:
-                #        self.databases[i].tables.pop(j)
-                #        break
-        os.remove(f"{self.directory}/{db_name}/{table_name}.bin")
+        # remove the object
+        if db_name in self.databases:
+            db = self.databases[db_name].tables
+            if table_name in db:
+                del db[table_name]
+            else:
+                logging.warning(f"DatabaseEngine: table {table_name} was not found in {db_name}. Table file {table_name}.bin may still exist in the db directory.")
+        else:
+            logging.warning(f"DatabaseEngine: db {db_name} was not registered. db folder {db_name} may still exist.")
+        # remove the file
+        try:
+            os.remove(f"{self.directory}/{db_name}/{table_name}.bin")
+        except OSError as e:
+            logging.error(f"{e}: table was not removed.")
         
     # completely delete the the database
     def drop_db(self, db_name: str):
-        for i in range(len(self.databases)):
-            if self.databases[i] == db_name:
-                self.databases.pop(i)
-                break
-        os.rmdir(self.directory+"/"+db_name)
+        if db_name in self.databases:
+            del self.databases[db_name]
+        else:
+            logging.warning(f"DatabaseEngine: database {db_name} was not registered")
+        try:
+            os.rmdir(self.directory+"/"+db_name)
+        except OSError as e:
+            logging.error(f"{e}: dataabse folder was not removed.")
+        
 
     # append data into table.bin as binary data
     def insert(self, db, table, data=[]):
