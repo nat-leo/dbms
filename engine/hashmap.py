826cb5a2445b2a86345a55300c2b3aac51b9f7a1
natleo <n8wyliu@gmail.com>
2023-08-24 15:07:48 -0700

    idk what I did to update, but the hashmap should have a list of keys.

--- engine/hashmap.py
+++ engine/hashmap.py
@@ -4,12 +4,15 @@ class Hashmap:
         self.pairs = {}
 
     def insert(self, key, value):
-        self.pairs[key] = value
+        if key in self.pairs:
+            self.pairs[key].append(value)
+        else:
+            self.pairs[key] = [value]
         self.size += 1
 
     def delete(self, key):
+        self.size -= len(self.pairs[key])
         del self.pairs[key]
-        self.size -= 1
 
     def clear(self):
         self.size = 0
