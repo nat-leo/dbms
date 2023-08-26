89b95fc867cd8a5962f555bce6f999375e40f462
natleo <n8wyliu@gmail.com>
2023-08-26 15:48:19 -0700

    add starting boilerplate for hashma datastructure.

--- engine/hashmap.py
+++ engine/hashmap.py
@@ -1,2 +1,16 @@
 class Hashmap:
-    pass
+    def __init__(self) -> None:
+        self.map = ()
+        self.size = 1
+
+    def __repr__(self) -> str:
+        pass
+
+    def search(self):
+        pass
+
+    def insert(self, val):
+        pass
+
+    def delete(self, val):
+        pass
