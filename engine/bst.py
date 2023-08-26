4451f919166a8f67fb2031bdefbb0ab814a4b45d
natleo <n8wyliu@gmail.com>
2023-08-26 15:47:08 -0700

    add starting boilerplate for bst data structure

--- engine/bst.py
+++ engine/bst.py
@@ -1,5 +1,25 @@
 class Node:
-    pass
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self) -> str:
+        return self.left.__repr__() + " " + self.right.__repr__() + " " + self.val
 
 class BinarySearchTree:
-    pass
+    def __init__(self) -> None:
+        self.root = None
+        self.size = 1
+
+    def __repr__(self) -> str:
+        return self.root.__repr__()
+
+    def search(self):
+        pass
+
+    def insert(self, val):
+        pass
+
+    def delete(self, val):
+        pass
