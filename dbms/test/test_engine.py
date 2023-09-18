5f1b87853d9142c78af0f1d1657e8333551e551e
natleo <n8wyliu@gmail.com>
2023-09-18 09:26:48 -0700

    add more descriptive header comment, add boiler plate unittest class for testing the database engine.

--- dbms/test/test_engine.py
+++ dbms/test/test_engine.py
@@ -1,31 +1,18 @@
-# Test File for Engine folder
+# Test File for dbms/engine
 import os
-import pytest
+import shutil
+import unittest
 from engine import engine
 
 ###############################################################################
 # engine/engine.py
 ###############################################################################
+class EngineTest(unittest.TestCase):
+    def setUp(self):
+        self.eng = engine.DatabaseEngine("test", "password")
 
-# New user created
-@pytest.fixture
-def new_user():
-    return engine.DatabaseEngine("test", "password")
+    def tearDown(self):
+        shutil.rmtree(self.eng.directory)
 
-# New user creates table
-@pytest.fixture
-def creates_table(new_user):
-    new_user.create_table("table", {})
-    return new_user
-
-# New users should have everything clear except a folder of the same name as the user
-def test_new_user_database_engine(new_user):
-    assert os.path.isdir(new_user.directory), "New user 'test' did not create a new directory"
-
-# New users should be able to create any new table without error
-def test_new_user_create_table(creates_table):
-    assert os.path.exists(creates_table.directory+"/table/data.bin"), "New table 'table' did not create a new file"
-    
-# Database object should have a new table in its tables attribute when user calls create table
-def test_new_user_table_in_db_object(creates_table):
-    assert "table" in creates_table.tables, "New table 'table' not created in database object"
+    def test_works(self):
+        print("hoorah.")
